f98302ce162ca06fdc033ab95db3d8efee76b6ce
zakharovava2023 <125122557+zakharovava2023@users.noreply.github.com>
2023-05-20 14:29:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,4 +7,20 @@
 
 BST<std::string> makeTree(const char* filename) {
   // поместите сюда свой код
+    BST<std::string> tree;
+    std::ifstream file(filename);
+    std::string word;
+    if (!file)
+        std::cout << "File error!" << std::endl;
+    while (!file.eof()) {
+        char ch = tolower(file.get());
+        if (isalpha(ch)) {
+            word += ch;
+        } else {
+            tree.add(word);
+            word.clear();
+        }
+    }
+    file.close();
+    return tree;
 }
